1645acc13d2b1d72b319289277e960aff1c42a25
pandillapujitha <pandillapujitha13@gmail.com>
2026-07-31 21:29:22 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function login() {
+    alert("Login Successful");
+}
+
+function register() {
+    alert("Registration Successful");
+}
+
+function bookTour() {
+    alert("Tour Booked Successfully");
+}
